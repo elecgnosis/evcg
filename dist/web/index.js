3f2720300db4c6351bd482c493570a8b37ab3049
unknown <elecgnosis@outlook.com>
2017-06-22 13:16:01 -0700

    feature complete

--- dist/web/index.js
+++ dist/web/index.js
@@ -5,27 +5,60 @@ var get = function _get(url, callback) {
   xhr.open('GET', url);
   xhr.onreadystatechange = function () {
     if (xhr.readyState > 3 && xhr.status === 200) {
-      callback(xhr.responseText);
+      callback(null, xhr.responseText);
     }
   };
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   xhr.send();
-
   return xhr;
 };
 
-document.addEventListener('DOMContentLoaded', function () {
+var submitZip = function _submitZip(zip) {
+  var result = document.querySelector('.result');
+  var city = document.querySelector('.city');
+  var temp = document.querySelector('.temp');
+  var timezone = document.querySelector('.timezone');
+  var elevation = document.querySelector('.elevation');
+
+  if (result.classList.contains('show')) {
+    result.classList.remove('show');
+    setTimeout(function () {
+      city.innerText = '';
+      temp.innerText = '';
+      timezone.innerText = '';
+      elevation.innerText = '';
+    }, 250);
+  }
+
+  if (zip && zip.length === 5) {
+    get('demo?zip=' + zip, function (xhr, res) {
+      if (res) {
+        var resParsed = JSON.parse(res);
+        city.innerText = resParsed.city;
+        temp.innerText = resParsed.temp;
+        timezone.innerText = resParsed.timezone;
+        elevation.innerText = resParsed.elevation;
+        result.classList.add('show');
+        return true;
+      }
+      return false;
+    });
+  }
+};
+
+var run = function _run() {
   var sendButton = document.querySelector('button.send');
   var zipInput = document.querySelector('input.zip');
-  sendButton.addEventListener('click', function () {
-    var zip = zipInput.value;
-    if (zip && zip.length === 5) {
-      get('demo?zip=98685', function (err, res) {
-        if (err) console.error(err);
-        if (res) {
-          console.log(res);
-        }
-      });
+
+  zipInput.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+      submitZip(zipInput.value);
     }
   });
-});
+  sendButton.addEventListener('click', function () {
+    submitZip(zipInput.value);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', run);
--- src/web/index.js
+++ src/web/index.js
@@ -3,27 +3,60 @@ const get = function _get(url, callback) {
   xhr.open('GET', url);
   xhr.onreadystatechange = () => {
     if (xhr.readyState > 3 && xhr.status === 200) {
-        callback(xhr.responseText);
+        callback(null, xhr.responseText);
     }
   };
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   xhr.send();
-
   return xhr;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+const submitZip = function _submitZip(zip) {
+  const result = document.querySelector('.result');
+  const city = document.querySelector('.city');
+  const temp = document.querySelector('.temp');
+  const timezone = document.querySelector('.timezone');
+  const elevation = document.querySelector('.elevation');
+
+  if (result.classList.contains('show')) {
+    result.classList.remove('show');
+    setTimeout(() => {
+      city.innerText = '';
+      temp.innerText = '';
+      timezone.innerText = '';
+      elevation.innerText = '';
+    }, 250);
+  }
+
+  if (zip && zip.length === 5) {
+    get(`demo?zip=${zip}`, (xhr, res) => {
+      if (res) {
+        const resParsed = JSON.parse(res);
+        city.innerText = resParsed.city;
+        temp.innerText = resParsed.temp;
+        timezone.innerText = resParsed.timezone;
+        elevation.innerText = resParsed.elevation;
+        result.classList.add('show');
+        return true;
+      }
+      return false;
+    });
+  }
+}
+
+const run = function _run() {
   const sendButton = document.querySelector('button.send');
   const zipInput = document.querySelector('input.zip');
-  sendButton.addEventListener('click', () => {
-    const zip = zipInput.value;
-    if (zip && zip.length === 5) {
-      get('demo?zip=98685', (err, res) => {
-        if (err) console.error(err);
-        if (res) {
-          console.log(res);  
-        }
-      });
+
+  zipInput.addEventListener("keyup", (event) => {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        submitZip(zipInput.value);
     }
   });
-});
+  sendButton.addEventListener('click', () => {
+    submitZip(zipInput.value);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', run);
